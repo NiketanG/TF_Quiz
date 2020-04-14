12bbcb5aa3c576460549cc1eb42484eed203f111
NiketanG <nikegulekar@gmail.com>
2020-04-14 13:56:39 +0530

    Fixed config

--- app/config.py
+++ app/config.py
@@ -4,6 +4,7 @@ class Config:
     EVENT = 'Webber'
     QUESTION_COUNT = 50
     SECRET_KEY = '57bde4dd-0da1-41dd-b5bc-38d5997d0fc1'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:N!kketanGT16@localhost/tf_quiz'
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    #'postgresql://postgres:N!kketanGT16@localhost/tf_quiz'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
